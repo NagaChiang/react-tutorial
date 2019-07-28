cc0fac1ae006a0aefc294226ed42f2b251c2b2d5
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2019-07-28 16:23:34 +0800

    Updates

---
+++ precache-manifest.4d453d727e4e5aeca566a463a861ea4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f401a05ec7d950cf871962d5ce5b62",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "99168806f12e0569ca21",
+    "url": "/react-tutorial/static/css/main.c6321d75.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/react-tutorial/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "99168806f12e0569ca21",
+    "url": "/react-tutorial/static/js/main.bf5006cb.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  }
+]);
